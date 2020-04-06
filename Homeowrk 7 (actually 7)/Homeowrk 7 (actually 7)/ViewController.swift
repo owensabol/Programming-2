020feba982ba648cd5971f2a68e6d6cf44602787
Owen Sabol <o.sabol@setonhill.edu>
2020-04-06 13:18:08 -0400

    done

---
+++ Homeowrk 7 (actually 7)/Homeowrk 7 (actually 7)/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Homeowrk 7 (actually 7)
+//
+//  Created by Owen Sabol on 3/31/20.
+//  Copyright © 2020 Owen Sabol. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    let locMan: CLLocationManager = CLLocationManager()
+    var startLocation: CLLocation!
+    
+    let pncparkLatitude: CLLocationDegrees = 40.4469
+    let pncparkLongitude: CLLocationDegrees = 80.0057
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation: CLLocation = locations[0]
+        NSLog("Something is happening")
+        
+    if newLocation.horizontalAccuracy >= 0 {
+        let pncPark:CLLocation = CLLocation(latitude: pncparkLatitude, longitude: pncparkLongitude)
+        let delta: CLLocationDistance = pncPark.distance(from: newLocation)
+        let miles: Double = (delta * 0.000621371) + 0.5
+        if miles < 3 {
+            locMan.stopUpdatingLocation()
+            distanceLabel.text = "Enjoy\nPNC Park!"
+        } else{
+            let commaDelimited = NumberFormatter()
+            commaDelimited.numberStyle = NumberFormatter.Style.decimal
+            distanceLabel.text=commaDelimited.string(from: NSNumber(value: miles))! + "miles to PNC Park!"
+        }
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        locMan.delegate = self
+        locMan.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locMan.distanceFilter = 1609; //mile in meters
+        locMan.requestWhenInUseAuthorization()
+        locMan.startUpdatingLocation()
+        startLocation = nil
+    
+    }
+
+
+}
+
+}
---
+++ Homeowrk 7 (actually 7)/Homeowrk 7 (actually 7)Tests/Homeowrk_7__actually_7_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Homeowrk_7__actually_7_Tests.swift
+//  Homeowrk 7 (actually 7)Tests
+//
+//  Created by Owen Sabol on 3/31/20.
+//  Copyright © 2020 Owen Sabol. All rights reserved.
+//
+
+import XCTest
+@testable import Homeowrk_7__actually_7_
+
+class Homeowrk_7__actually_7_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
